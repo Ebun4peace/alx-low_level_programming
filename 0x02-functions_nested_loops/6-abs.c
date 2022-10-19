0b3120742955a3105322bee29a0717401ece92fa
Ebun <ebun.adereti@gmail.com>
2022-10-18 19:16:31 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ *_abs - computes the absolute value of an integer
+* @n: integer we are looking for
+* Return: the abs of the integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
