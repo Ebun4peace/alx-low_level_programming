040d66629dce8aa501b131d92d6294926abbff60
Ebun <ebun.adereti@gmail.com>
2022-10-18 18:12:19 -0700

    task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * print_alphabet_x10 - print alphabeth in lower case
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
-	
+
 	while (count++ <= 9)
 	{
 	for (letter = 'a'; letter >= 'z'; letter++)
