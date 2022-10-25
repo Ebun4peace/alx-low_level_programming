0dfdd7a1def3b30afcef59281c84551d97f2c822
Ebun <ebun.adereti@gmail.com>
2022-10-24 17:26:20 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - Function to get the length of the string
+ * @s: string pointer to pass to this string
+ * Return: Returns length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
