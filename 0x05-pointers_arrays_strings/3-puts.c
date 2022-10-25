47572f9e2483605895b8f8efec68256c65937936
Ebun <ebun.adereti@gmail.com>
2022-10-24 17:54:14 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that puts
+ * @str: a param to _puts function
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'l str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
